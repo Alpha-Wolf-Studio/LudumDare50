a33ff4ce623f762169d4e1f94a26df896704de7e
Federico Olive <FedericoOlive@gmail.com>
2022-04-02 12:05:51 -0300

    Integrate Main Menu Scene.

--- Assets/Scripts/Ui/UiMainMenuManager.cs
+++ Assets/Scripts/Ui/UiMainMenuManager.cs
@@ -1,30 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class UiMainMenuManager : MonoBehaviour
 {
+    [SerializeField] private float transitionTime;
+    [SerializeField] private CanvasGroup[] menues;
 
-
-    // Start is called before the first frame update
-    void Start()
+    enum Menu
     {
-        
+        Main,
+        Settings,
+        Credits
     }
+    private Menu menu = Menu.Main;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        foreach (CanvasGroup menu in menues)
+        {
+            menu.blocksRaycasts = false;
+            menu.interactable = false;
+            menu.alpha = 0;
+        }
+
+        menues[(int) Menu.Main].interactable = true;
+        menues[(int)Menu.Main].blocksRaycasts = true;
+        menues[(int)Menu.Main].alpha = 1;
     }
-
-    public void ButtonPlay()
+    public void OnButtonPlay()
     {
-
+        SceneManager.LoadScene(1);
     }
-
-    public void ButtonSetting()
+    public void OnButtonSetting()
     {
+        StartCoroutine(SwitchPanel(transitionTime, (int) Menu.Settings, (int) Menu.Main));
+    }
+    public void OnButtonBackSettings()
+    {
+        StartCoroutine(SwitchPanel(transitionTime, (int) Menu.Main, (int)Menu.Settings));
+    }
+    public void OnButtonCredits()
+    {
+        StartCoroutine(SwitchPanel(transitionTime, (int)Menu.Credits, (int)Menu.Main));
+    }
+    public void OnButtonBackCredits()
+    {
+        StartCoroutine(SwitchPanel(transitionTime, (int)Menu.Main, (int)Menu.Credits));
+    }
+    public void OnButtonExit()
+    {
+        Application.Quit(0);
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+    }
+    IEnumerator SwitchPanel(float maxTime, int onMenu, int offMenu)
+    {
+        float onTime = 0;
+        CanvasGroup on = menues[onMenu];
+        CanvasGroup off = menues[offMenu];
 
+        off.blocksRaycasts = false;
+        off.interactable = false;
+
+        while (onTime < maxTime)
+        {
+            onTime += Time.deltaTime;
+            float fade = onTime / maxTime;
+            on.alpha = fade;
+            off.alpha = 1 - fade;
+            yield return null;
+        }
+        on.blocksRaycasts = true;
+        on.interactable = true;
+        onTime = 0;
+
+        menu = (Menu)onMenu;
     }
 }
