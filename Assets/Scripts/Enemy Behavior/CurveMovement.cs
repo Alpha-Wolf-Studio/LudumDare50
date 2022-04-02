618cbf7af52fce9126c58581e9f3a92af11ad773
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-04-02 02:45:39 -0300

    Added curve movement script.

--- Assets/Scripts/Enemy Behavior/CurveMovement.cs
+++ Assets/Scripts/Enemy Behavior/CurveMovement.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
-public class ForwardMovement : MonoBehaviour
+public class CurveMovement : MonoBehaviour
 {
     [SerializeField] float lerpTime = 1f;
     [SerializeField] float currentLerpTime;
     [SerializeField] bool isLerping;
 
-    [SerializeField] Vector3 startPos;
+    Vector3 startPos;
     [SerializeField] Vector3 endPos;
 
+    float startTime;
+    Vector3 centerPoint;
+    Vector3 startRelCenter;
+    Vector3 endRelCenter;    
+
     private Rigidbody2D rb;
-    
+
     // Start is called before the first frame update
 
     private void Awake()
@@ -27,8 +32,11 @@ public class ForwardMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void FixedUpdate()
     {
+        GetCenter(Vector3.up);
+
         if (isLerping)
         {
             currentLerpTime += Time.deltaTime;
@@ -41,7 +49,16 @@ public class ForwardMovement : MonoBehaviour
 
             float percentComplete = currentLerpTime / lerpTime;
             Debug.Log(percentComplete);
-            rb.MovePosition(Vector3.Lerp(startPos, endPos, percentComplete));
+
+            rb.MovePosition(Vector3.Slerp(startRelCenter, endRelCenter, percentComplete));
         }
+    }
+
+    void GetCenter(Vector3 direction)
+    {
+        centerPoint = (startPos + endPos) * 0.5f;
+        centerPoint -= direction;
+        startRelCenter = startPos - centerPoint;
+        endRelCenter = endPos - centerPoint;
     }
 }
