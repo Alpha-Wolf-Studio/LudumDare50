cf42c73959adf73776d66b2efaff7518dfc7efb2
Owinmowe <Owinmowe@gmail.com>
2022-04-02 12:39:34 -0300

    Updated GameManager with scene.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class LevelsTimeConfigurations 
+    {
+        public float timeForNextLevel;
+    }
+
+    public System.Action OnNewLevelStarted;
 
     [Header("References")]
     [SerializeField] private EnemyManager enemyManager;
     [SerializeField] ShipPlayer player;
     [Header("Configuration")]
-    [SerializeField] private int levelsAmount = 5;
+    [SerializeField] private List<LevelsTimeConfigurations> timesConfigurations;
 
-    private int currentLevel = 0;
+    private int currentLevel = -1;
+    private float currentLevelTime = 0;
+    private IEnumerator timeIEnumerator;
 
     public void StartNewLevel() 
     {
-        currentLevel++;
-        if (currentLevel == levelsAmount) currentLevel = 0;
+        IncreaseCurrentLevel();
         SetCurrentLevel();
         StartCurrentLevel();
+    }
+
+    private void IncreaseCurrentLevel() 
+    {
+        currentLevel++;
+        if (currentLevel == timesConfigurations.Count)
+        {
+            currentLevel = 0;
+            currentLevelTime = 0;
+        }
     }
 
     private void SetCurrentLevel() 
@@ -29,11 +47,21 @@ public class GameManager : MonoBehaviour
     private void StartCurrentLevel()
     {
         enemyManager.StartCurrentLevel();
+
+        if (timeIEnumerator != null) StopCoroutine(timeIEnumerator);
+        timeIEnumerator = TimeCoroutine();
+        StartCoroutine(timeIEnumerator);
+
+        OnNewLevelStarted?.Invoke();
     }
 
-    public void SetTimeScale(float time) 
+    private IEnumerator TimeCoroutine() 
     {
-
+        while (currentLevelTime < timesConfigurations[currentLevel].timeForNextLevel) 
+        {
+            currentLevelTime += Time.deltaTime;
+            yield return null;
+        }
+        StartNewLevel();
     }
-
 }
