25ee21a507abf2b44e808a674f8df6b8804a9363
Owinmowe <Owinmowe@gmail.com>
2022-04-03 02:19:23 -0300

    Modified enemy manager to use a custom class instead of several spawn lists.

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -15,13 +15,93 @@ public class EnemyManager : MonoBehaviour
         public float spawnAccelerationSpeed;
 
         [Header("Enemies References")]
-        public List<GameObject> airEnemiesPrefab;
-        public List<GameObject> airSideEnemiesPrefab;
-        public List<GameObject> onWaterEnemiesPrefab;
-        public List<GameObject> underWaterEnemiesPrefab;
+        public List<EnemySpawn> enemiesPrefab;
     }
 
-    private enum SpawnType { UNDERWATER, ONWATER, AIR, AIR_SIDE, SIZE}
+    [System.Serializable]
+    public class EnemySpawn 
+    {
+        public enum SpawnType { UNDERWATER, ONWATER, AIR, AIR_SIDE, AIR_CENTER, WATER_CENTER}
+        public SpawnType currentSpawnType;
+        public GameObject prefab;
+
+        public Vector3 GetSpawnPosition(Vector3 playerPosition) 
+        {
+
+            Camera camera = Camera.main;
+
+            Vector2 minCamera = camera.ScreenToWorldPoint(new Vector2(0, 0));
+            Vector2 maxCamera = camera.ScreenToWorldPoint(new Vector2(camera.pixelWidth, camera.pixelHeight));
+
+            Vector3 newSpawnPosition = playerPosition;
+            Vector3 EnemyGraphicBoundsExtents = Vector3.zero;
+
+            switch (currentSpawnType)
+            {
+                case SpawnType.UNDERWATER:
+
+                    var underWaterSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (underWaterSpriteRenderer) EnemyGraphicBoundsExtents = underWaterSpriteRenderer.bounds.extents;
+
+                    newSpawnPosition.y += minCamera.y - EnemyGraphicBoundsExtents.y;
+                    newSpawnPosition.x += Random.Range(minCamera.x, maxCamera.x);
+
+                    break;
+
+
+                case SpawnType.ONWATER:
+
+                    var onWaterSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (onWaterSpriteRenderer) EnemyGraphicBoundsExtents = onWaterSpriteRenderer.bounds.extents;
+
+                    int randomDirection = Random.Range(0, 2);
+                    newSpawnPosition.x += randomDirection == 0 ? minCamera.x - EnemyGraphicBoundsExtents.x : maxCamera.x + EnemyGraphicBoundsExtents.x;
+
+                    break;
+
+
+                case SpawnType.AIR:
+
+                    var airSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (airSpriteRenderer) EnemyGraphicBoundsExtents = airSpriteRenderer.bounds.extents;
+
+                    newSpawnPosition.y += maxCamera.y + EnemyGraphicBoundsExtents.y;
+                    newSpawnPosition.x += Random.Range(minCamera.x, maxCamera.x);
+
+                    break;
+
+
+                case SpawnType.AIR_SIDE:
+
+                    var airSideSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (airSideSpriteRenderer) EnemyGraphicBoundsExtents = airSideSpriteRenderer.bounds.extents;
+
+                    int newRandomDirection = Random.Range(0, 2);
+                    newSpawnPosition.x += newRandomDirection == 0 ? minCamera.x - EnemyGraphicBoundsExtents.x : maxCamera.x + EnemyGraphicBoundsExtents.x;
+                    newSpawnPosition.y = maxCamera.y - EnemyGraphicBoundsExtents.y;
+
+                    break;
+
+                case SpawnType.AIR_CENTER:
+                    var airCenterSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (airCenterSpriteRenderer) EnemyGraphicBoundsExtents = airCenterSpriteRenderer.bounds.extents;
+                    newSpawnPosition.x = Mathf.Lerp(minCamera.x, maxCamera.x, 0.5f);
+                    newSpawnPosition.y = maxCamera.y + EnemyGraphicBoundsExtents.y;
+                    break;
+
+                case SpawnType.WATER_CENTER:
+                    var waterCenterSpriteRenderer = prefab.GetComponent<SpriteRenderer>();
+                    if (waterCenterSpriteRenderer) EnemyGraphicBoundsExtents = waterCenterSpriteRenderer.bounds.extents;
+                    newSpawnPosition.x = Mathf.Lerp(minCamera.x, maxCamera.x, 0.5f);
+                    newSpawnPosition.y = maxCamera.y - EnemyGraphicBoundsExtents.y;
+                    break;
+
+                default:
+                    break;
+            }
+            return newSpawnPosition;
+        }
+    }
 
     [SerializeField] private List<LevelSpawnConfigurations> configurations;
 
@@ -86,125 +166,17 @@ public class EnemyManager : MonoBehaviour
             Debug.LogWarning("EnemyManager sin configuracion actual encontrada. Se setea la primera de la lista");
         }
 
-        Vector3 newSpawnPosition = currentPlayer.transform.position;
+        if (currentConfiguration.enemiesPrefab == null) return;
+        int enemyIndex = Random.Range(0, currentConfiguration.enemiesPrefab.Count);
+        var enemyToSpawn = currentConfiguration.enemiesPrefab[enemyIndex];
 
-        SpawnType spawnType = (SpawnType)Random.Range(0, (int)SpawnType.SIZE);
+        Vector3 newSpawnPosition = enemyToSpawn.GetSpawnPosition(currentPlayer.transform.position);
 
-        var valid = CheckIfTypeIsValid(spawnType);
-        if (valid == typeValidity.COMPLETE_NULL) return;
-
-        while (CheckIfTypeIsValid(spawnType) != typeValidity.OK)
-        {
-            spawnType = (SpawnType)Random.Range(0, (int)SpawnType.SIZE);
-        }
-
-        Camera camera = Camera.main;
-
-        Vector2 minCamera = camera.ScreenToWorldPoint(new Vector2(0, 0));
-        Vector2 maxCamera = camera.ScreenToWorldPoint(new Vector2(camera.pixelWidth, camera.pixelHeight));
-
-        GameObject enemyToSpawn = null;
-        Vector3 EnemyGraphicBoundsExtents = Vector3.zero;
-
-        switch (spawnType)
-        {
-            case SpawnType.UNDERWATER:
-
-                int underWaterSpawnIndex = Random.Range(0, currentConfiguration.underWaterEnemiesPrefab.Count);
-                enemyToSpawn = currentConfiguration.underWaterEnemiesPrefab[underWaterSpawnIndex];
-
-                var underWaterSpriteRenderer = enemyToSpawn.GetComponent<SpriteRenderer>();
-                if(underWaterSpriteRenderer) EnemyGraphicBoundsExtents = underWaterSpriteRenderer.bounds.extents;
-
-                newSpawnPosition.y += minCamera.y - EnemyGraphicBoundsExtents.y;
-                newSpawnPosition.x += Random.Range(minCamera.x, maxCamera.x);
-
-                break;
-
-            case SpawnType.ONWATER:
-
-                int onWaterSpawnIndex = Random.Range(0, currentConfiguration.onWaterEnemiesPrefab.Count);
-                enemyToSpawn = currentConfiguration.onWaterEnemiesPrefab[onWaterSpawnIndex];
-
-                var onWaterSpriteRenderer = enemyToSpawn.GetComponent<SpriteRenderer>();
-                if (onWaterSpriteRenderer) EnemyGraphicBoundsExtents = onWaterSpriteRenderer.bounds.extents;
-
-                int randomDirection = Random.Range(0, 2);
-                newSpawnPosition.x += randomDirection == 0 ? minCamera.x - EnemyGraphicBoundsExtents.x : maxCamera.x + EnemyGraphicBoundsExtents.x;
-
-
-                break;
-
-            case SpawnType.AIR:
-
-                int airSpawnIndex = Random.Range(0, currentConfiguration.airEnemiesPrefab.Count);
-                enemyToSpawn = currentConfiguration.airEnemiesPrefab[airSpawnIndex];
-
-                var airSpriteRenderer = enemyToSpawn.GetComponent<SpriteRenderer>();
-                if (airSpriteRenderer) EnemyGraphicBoundsExtents = airSpriteRenderer.bounds.extents;
-
-                newSpawnPosition.y += maxCamera.y + EnemyGraphicBoundsExtents.y;
-                newSpawnPosition.x += Random.Range(minCamera.x, maxCamera.x);
-
-                break;
-
-            case SpawnType.AIR_SIDE:
-
-                int airSideSpawnIndex = Random.Range(0, currentConfiguration.airSideEnemiesPrefab.Count);
-                enemyToSpawn = currentConfiguration.airSideEnemiesPrefab[airSideSpawnIndex];
-
-                var airSideSpriteRenderer = enemyToSpawn.GetComponent<SpriteRenderer>();
-                if (airSideSpriteRenderer) EnemyGraphicBoundsExtents = airSideSpriteRenderer.bounds.extents;
-
-                int newRandomDirection = Random.Range(0, 2);
-                newSpawnPosition.x += newRandomDirection == 0 ? minCamera.x - EnemyGraphicBoundsExtents.x : maxCamera.x + EnemyGraphicBoundsExtents.x;
-                newSpawnPosition.y = maxCamera.y - EnemyGraphicBoundsExtents.y;
-
-                break;
-
-            default:
-                break;
-        }
-
-        var enemyGameobject = Instantiate(enemyToSpawn, newSpawnPosition, Quaternion.identity, transform);
+        var enemyGameobject = Instantiate(enemyToSpawn.prefab, newSpawnPosition, Quaternion.identity, transform);
 
         var enemyMovement = enemyGameobject.GetComponent<MovementBase>();
         if(enemyMovement) enemyMovement.SetNewTarget(currentPlayer.transform);
 
         
     }
-
-    enum typeValidity { COMPLETE_NULL, TYPE_NULL, OK}
-
-    private typeValidity CheckIfTypeIsValid(SpawnType type) 
-    {
-        int underWaterAmount = currentConfiguration.underWaterEnemiesPrefab.Count;
-        int onWaterAmount = currentConfiguration.onWaterEnemiesPrefab.Count;
-        int airAmount = currentConfiguration.airEnemiesPrefab.Count;
-        int airSideAmount = currentConfiguration.airSideEnemiesPrefab.Count;
-
-        int fullAmount = underWaterAmount + onWaterAmount + airAmount + airSideAmount;
-
-        if (fullAmount == 0) return typeValidity.COMPLETE_NULL;
-
-
-        switch (type)
-        {
-            case SpawnType.UNDERWATER:
-                if (currentConfiguration.underWaterEnemiesPrefab.Count > 0) return typeValidity.OK;
-                break;
-            case SpawnType.ONWATER:
-                if (currentConfiguration.onWaterEnemiesPrefab.Count > 0) return typeValidity.OK;
-                break;
-            case SpawnType.AIR:
-                if (currentConfiguration.airEnemiesPrefab.Count > 0) return typeValidity.OK;
-                break;
-            case SpawnType.AIR_SIDE:
-                if (currentConfiguration.airSideEnemiesPrefab.Count > 0) return typeValidity.OK;
-                break;
-        }
-
-        return typeValidity.TYPE_NULL;
-    }
-
 }
