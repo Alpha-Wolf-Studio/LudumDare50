7d17285698bc2f3b809657a02593cb942e49acc8
Federico Olive <FedericoOlive@gmail.com>
2022-04-03 14:36:31 -0300

    Updale panel Settings and Sounds.

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,67 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviourSingleton<AudioManager>
 {
-    [SerializeField] private ShipPlayer player;
-    [SerializeField] private AudioSource musicSource;
+    private UiSliderRef sliders;    // Se busca automaticamente
 
     public AudioMixer audioMixer;
 
-    private float maxVol = 20;
-    private float minVol = -30;
-    private float distVol;
+    [SerializeField] private float lastVolMusic;
+    [SerializeField] private float lastVolEffect;
+    [SerializeField] private bool isMusic = true;
+    [SerializeField] private bool isEffect = true;
 
-    private UiSliderRef sliders;
-    private List<float> sliderValue = new List<float>();
-
-    public enum Sounds     // Esto se ordena dependiendo del array objectsSounds !!
+    public void OnStart()
     {
-        OnDie,
-        OnKillEnemies
-    }
-    public Sounds soundsOrder;
-    [SerializeField] private AudioSource[] objectsSounds;
+        FindUiSliderRef();
+        SetSliderValue();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        sliderValue.Add(7);
-        sliderValue.Add(6);
+        sliders.buttonMusic.image.color = isMusic ? Color.green : Color.red;
+        sliders.buttonEffect.image.color = isEffect ? Color.green : Color.red;
     }
-
     void SetSliderValue()
     {
-        for (int i = 0; i < sliders.sliders.Length; i++)
-        {
-            sliders.sliders[i].value = sliderValue[i];
-        }
-    }
+        float volume = 0;
 
+        audioMixer.GetFloat("VolMusic", out volume);
+        sliders.sliderMusic.value = volume;
+
+        audioMixer.GetFloat("VolEffect", out volume);
+        sliders.sliderEffect.value = volume;
+    }
     public void FindUiSliderRef()
     {
         sliders = FindObjectOfType<UiSliderRef>();
         Invoke(nameof(SetSliderValue), 0.1f);
     }
-
-    
-    public void SetEffectVolume(float volume)
-    {
-        //distVol = Mathf.Abs(maxVol - minVol) / 10;
-        //if (volume < 1)
-        //{
-        //    audioMixer[0].SetFloat("MasterVolume", -80);
-        //}
-        //else
-        //{
-        //    audioMixer[0].SetFloat("MasterVolume", minVol + volume * distVol);
-        //}
-    }
     public void SetMusicVolume(float volume)
     {
-      
+        lastVolMusic = volume;
+        audioMixer.SetFloat("VolMusic", volume);
     }
-
+    public void SetEffectVolume(float volume)
+    {
+        lastVolEffect = volume;
+        audioMixer.SetFloat("VolEffect", volume);
+    }
+    public void AlternateAudioMusic()
+    {
+        isMusic = !isMusic;
+        audioMixer.SetFloat("VolMusic", isMusic ? lastVolMusic : -80);
+        sliders.buttonMusic.image.color = isMusic ? Color.green : Color.red;
+    }
+    public void AlternateAudioEffect()
+    {
+        isEffect = !isEffect;
+        audioMixer.SetFloat("VolEffect", isEffect ? lastVolEffect : -80);
+        sliders.buttonEffect.image.color = isEffect ? Color.green : Color.red;
+    }
 }
--- Assets/Scripts/Music Settings/UiSliderRef.cs
+++ Assets/Scripts/Music Settings/UiSliderRef.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UiSliderRef : MonoBehaviour   //Añadir script al canvas y pasarle los sliders
+public class UiSliderRef : MonoBehaviour
 {
-    public Slider[] sliders;
+    public Slider sliderMusic;
+    public Slider sliderEffect;
+
+    public Button buttonMusic;
+    public Button buttonEffect;
+
+    private void Start()
+    {
+        AudioManager.Get().OnStart();
+    }
+
+    // Se llaman desde los botones:
+
+    public void SetMusicVolume(float volume) => AudioManager.Get().SetMusicVolume(volume);
+    public void SetEffectVolume(float volume) => AudioManager.Get().SetEffectVolume(volume);
+    public void AlternateAudioMusic() => AudioManager.Get().AlternateAudioMusic();
+    public void AlternateAudioEffect() => AudioManager.Get().AlternateAudioEffect();
+
+    // ---------------------------
 }
