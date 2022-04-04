52016b7c898530d3ad67d4f2c11ff9e987d7088b
Owinmowe <Owinmowe@gmail.com>
2022-04-04 09:03:09 -0300

    Improved Look at script.

--- Assets/Scripts/Entities/Enemy/LookAt2D.cs
+++ Assets/Scripts/Entities/Enemy/LookAt2D.cs
@@ -4,44 +4,56 @@ using UnityEngine;
 
 public class LookAt2D : MonoBehaviour
 {
-
-    [SerializeField] bool invertX = false;
-    [SerializeField] forwardAligment axisDirection = forwardAligment.FRONT;
+    [SerializeField] private forwardAligment axisDirection = forwardAligment.FRONT;
     enum forwardAligment { FRONT, BACK, UP, DOWN}
 
     private Transform target = null;
     public void SetTarget(Transform target) => this.target = target;
 
+    private SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
         if (!target) return;
         var dir = target.position - transform.position;
-
+        dir.Normalize();
 
         switch (axisDirection)
         {
             case forwardAligment.FRONT:
-                if (invertX) transform.right = dir.normalized;
-                else transform.up = dir.normalized;
+                transform.right = dir.normalized;
 
                 break;
             case forwardAligment.BACK:
-                if (invertX) transform.right = -dir.normalized;
-                else transform.up = -dir.normalized;
+                transform.right = -dir.normalized;
 
                 break;
             case forwardAligment.UP:
-                if (invertX) transform.right = Vector3.Cross(Vector3.forward, dir.normalized);
-                else transform.up = Vector3.Cross(Vector3.forward, dir.normalized);
+                transform.up = dir.normalized;
 
                 break;
 
             case forwardAligment.DOWN:
-                if (invertX) transform.right = Vector3.Cross(-Vector3.forward, dir.normalized);
-                else transform.up = Vector3.Cross(-Vector3.forward, dir.normalized);
+                transform.up = -dir.normalized;
                 break;
             default:
                 break;
+        }
+
+        bool invert = transform.position.x < target.position.x;
+
+        if (invert)
+        {
+            renderer.flipY = true;
+        }
+        else 
+        {
+            renderer.flipY = false;
         }
     }
 }
